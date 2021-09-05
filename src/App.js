6b4e177a75a517be50e393afa7a4b5edb983cd36
Saddam-tech <salokhiddinov0727@gmail.com>
2021-09-06 08:01:40 +0900

    latest changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import { Switch, Route } from 'react-router-dom';
+import React, { useContext, Suspense } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import Layout from "./components/Layout/Layout";
+import AuthContext from "./store/auth-context";
+// import UserProfile from "./components/Profile/UserProfile";
+// import AuthPage from "./pages/AuthPage";
+// import HomePage from "./pages/HomePage";
+
+const UserProfile = React.lazy(() =>
+  import("./components/Profile/UserProfile")
+);
+const AuthPage = React.lazy(() => import("./pages/AuthPage"));
+const HomePage = React.lazy(() => import("./pages/HomePage"));
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
-    <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <HomePage />
-        </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
-        </Route>
-      </Switch>
-    </Layout>
+    <Suspense fallback={<p>Loading...</p>}>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          {!authCtx.isLoggedIn && (
+            <Route path="/auth">
+              <AuthPage />
+            </Route>
+          )}
+          <Route path="/profile">
+            {authCtx.isLoggedIn && <UserProfile />}
+            {!authCtx.isLoggedIn && <Redirect to="/auth" />}
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Layout>
+    </Suspense>
   );
 }
 
--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -25,22 +25,75 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
     // optional: Add validation
+
+    setIsLoading(true);
+    let url;
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBWWb-HVUL-Nh-qSb3zxJY6fTMSzuPLQtk";
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBWWb-HVUL-Nh-qSb3zxJY6fTMSzuPLQtk";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = "Authentication failed!";
+            // if (data && data.error & data.error.message) {
+            errorMessage = data.error.message;
+
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        const expirationTime = new Date(
+          new Date().getTime() + +data.expiresIn * 1000
+        );
+        authCtx.login(data.idToken, expirationTime.toISOString());
+        history.replace("/");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
   };
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required />
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required />
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
           <button
             type="button"
             className={classes.toggle}
